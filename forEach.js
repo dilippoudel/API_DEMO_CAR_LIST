e9c3641df83232145b81b8da637aeab4911a394a
dilippoudel <37371839+dilippoudel@users.noreply.github.com>
2018-12-14 05:25:48 +0200

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,6 +1,11 @@
 // Exercises 1
 // Write a function called printFirstAndLast which accepts an array (of objects) 
 // and console.logs a new string with the first character and the last character of each value.
+//This code gives the output in console as: 
+//ae
+//ee
+//of
+//fh
 function printFirstAndLast() {
     var arr = ['awesome','example','of','forEach'];
 arr.forEach(function(val, index, arr){
