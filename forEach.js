310453c67bc8d0058a219075edfc43dc856c4057
dilippoudel <37371839+dilippoudel@users.noreply.github.com>
2018-12-14 05:31:27 +0200

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,3 +1,12 @@
+//forEach is a Javascript iterator method which is used in the place of for loop normally. This iterator is a best option if you 
+// are not going to return the value. for eg 
+var arr = [1,2,3,4];
+var doubledValues = arr.forEach(function(val,index,arr){
+    return val*2;
+});
+doubledValues; //Undefined
+
+
 // Exercises 1
 // Write a function called printFirstAndLast which accepts an array (of objects) 
 // and console.logs a new string with the first character and the last character of each value.
